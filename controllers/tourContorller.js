b88d26f169ea3df948121bfee7b6aa82175b2814
ishaksebsib <ishaksebsib@gmail.com>
2023-05-08 00:49:55 +0300

    QUERY FILTERING

--- controllers/tourContorller.js
+++ controllers/tourContorller.js
@@ -2,11 +2,17 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+    // BUILD QUERY
+
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "list", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const tours = await Tour.find(queryObj);
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+
+    const tours = await query;
     res.status(200).json({
       status: "success",
       results: tours.length,
