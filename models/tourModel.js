bf9ef42d8e8b292402c4774681230935f0127230
ishaksebsib <ishaksebsib@gmail.com>
2023-05-07 22:38:52 +0300

    Update Tour Model Schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -23,15 +23,55 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "A TORU MUST HAVE A NAME !!!"],
+    trim: true,
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A TORU MUST HAVE A DURATION !!!"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A TORU MUST HAVE A MAX GROUP SIZE !!!"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A TORU MUST HAVE A DIFFICULTY !!!"],
+  },
+  ratingsAvrage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "A TOUR MUST HAVE A PRICE !!!"],
+  },
+  priceDiscount: { type: Number },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, " A TOUR MUST HAVE A SUMMARY   "],
+  },
+
+  descritption: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, " A TROU MUST HAVE A COVER IMAGE"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: {
+    type: Date,
   },
 });
 
