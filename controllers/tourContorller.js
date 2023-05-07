44fcbf0faae156b12f0ad5cf25e99304e1ebe6ac
ishaksebsib <ishaksebsib@gmail.com>
2023-05-08 00:44:56 +0300

    frittering by query

--- controllers/tourContorller.js
+++ controllers/tourContorller.js
@@ -2,7 +2,11 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "list", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
     res.status(200).json({
       status: "success",
       results: tours.length,
