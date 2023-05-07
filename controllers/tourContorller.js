f29e646a3bc2894747d82e590a30b875d788786f
ishaksebsib <ishaksebsib@gmail.com>
2023-05-07 12:03:09 +0300

    Create Mongodb Mongoose

--- controllers/tourContorller.js
+++ controllers/tourContorller.js
@@ -1,13 +1,21 @@
 const Tour = require("../models/tourModel");
 
-exports.getAllTours = (req, res) => {
-  // res.status(200).json({
-  //   status: "success",
-  //   results: tours.length,
-  //   data: {
-  //     tours,
-  //   },
-  // });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.getTourById = (req, res) => {
